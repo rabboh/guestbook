d324332523f80623d99bb4b737e1f9d34de1edac
rabboh <rabboh@yahoo.com>
2023-03-04 17:32:53 +0100

    folytatás

--- guestbook/guestbook/urls.py
+++ guestbook/guestbook/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from guestapi.urls import urlpatterns as guestapi_urls
+from webapp.urls import urlpatterns as webapp_urls
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/', include(guestapi_urls))
+    path('api/', include(guestapi_urls)),
+    path('', include(webapp_urls))
 ]
